e1827074394dc7fd370d8e84b3f4287a77c5b40c
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-26 10:12:16 +0300

    Added stripe

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {
   selectCartItems,
   selectCartTotal,
@@ -38,6 +39,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     ))}
 
     <div className="total">TOTAL: ${total}</div>
+
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+    </div>
+
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
